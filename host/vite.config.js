b4e4a3199a7102bca97901c351956f7cade7e0ba
Oleksandr-Melnyk-Artelogic <omelnyk@artelogic.net>
2024-11-27 18:10:21 +0200

    host vite config

--- host/vite.config.js
+++ host/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		federation({
+			name: 'host',
+			remotes: {
+				remote: 'http://localhost:5001/assets/remoteEntry.js',
+			},
+			shared: ['react', 'react-dom'],
+		}),
+	],
 })
